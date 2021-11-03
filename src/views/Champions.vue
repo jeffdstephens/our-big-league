84b09d6d3238e074d1cbed6149d759219d742cc2
jeff-stephens-dignari <jeff.stephens@dignari.com>
2021-11-02 20:42:00 -0400

    Completed initial rendering of championship teams without sorting or persistence

--- src/views/Champions.vue
+++ src/views/Champions.vue
@@ -1,8 +1,7 @@
 <template>
   <div>
     <h1>OBL Champions</h1>
-    <h1 v-for="team in $route.query.teamdetails" :key="team.id">{{team.name}}</h1>
-    <!-- <b-container>
+    <b-container>
       <b-table-simple hover small caption-top responsive>
         <b-thead head-variant="dark">
           <b-tr>
@@ -12,181 +11,21 @@
           </b-tr>
         </b-thead>
         <b-tbody>
-          <b-tr class="align-middle">
-            <b-td>Hampton Ballers</b-td>
-            <b-td>7</b-td>
+          <b-tr class="align-middle" v-for="team in $route.query.teamdetails" :key="team.id">
+            <b-td>{{team.name}}</b-td>
+            <b-td>{{team.appearances}}</b-td>
             <b-td>
-              <figure>
-                <img src="../assets/trophy.png" />
-                <figcaption>2001</figcaption>
-              </figure>
-              <figure>
-                <img src="../assets/trophy.png" />
-                <figcaption>2003</figcaption>
-              </figure>
-              <figure>
-                <img src="../assets/trophy.png" />
-                <figcaption>2008</figcaption>
-              </figure>
-              <figure>
-                <img src="../assets/trophy.png" />
-                <figcaption>2019</figcaption>
-              </figure>
-              <figure>
-                <img src="../assets/trophy.png" />
-                <figcaption>2020</figcaption>
-              </figure>
+              <span v-for="championshipYear in team.championships" :key="championshipYear">
+                <figure>
+                  <img src="../assets/trophy.png" width="65%" />
+                  <figcaption>{{ championshipYear }}</figcaption>
+                </figure>
+              </span>
             </b-td>
-          </b-tr>
-          <b-tr class="align-middle">
-            <b-td>Birrrrdy</b-td>
-            <b-td>7</b-td>
-            <b-td>
-              <figure>
-                <img src="../assets/trophy.png" />
-                <figcaption>1999</figcaption>
-              </figure>
-              <figure>
-                <img src="../assets/trophy.png" />
-                <figcaption>2005</figcaption>
-              </figure>
-              <figure>
-                <img src="../assets/trophy.png" />
-                <figcaption>2006</figcaption>
-              </figure>
-              <figure>
-                <img src="../assets/trophy.png" />
-                <figcaption>2016</figcaption>
-              </figure>
-            </b-td>
-          </b-tr>
-          <b-tr class="align-middle">
-            <b-td>Skindeep Ballaz</b-td>
-            <b-td>6</b-td>
-            <b-td>
-              <figure>
-                <img src="../assets/trophy.png" />
-                <figcaption>2002</figcaption>
-              </figure>
-              <figure>
-                <img src="../assets/trophy.png" />
-                <figcaption>2004</figcaption>
-              </figure>
-              <figure>
-                <img src="../assets/trophy.png" />
-                <figcaption>2009</figcaption>
-              </figure>
-              <figure>
-                <img src="../assets/trophy.png" />
-                <figcaption>2018</figcaption>
-              </figure>
-            </b-td>
-          </b-tr>
-          <b-tr class="align-middle">
-            <b-td>AMW</b-td>
-            <b-td>3</b-td>
-            <b-td>
-              <figure>
-                <img src="../assets/trophy.png" />
-                <figcaption>2010</figcaption>
-              </figure>
-              <figure>
-                <img src="../assets/trophy.png" />
-                <figcaption>2014</figcaption>
-              </figure>
-            </b-td>
-          </b-tr>
-          <b-tr class="align-middle">
-            <b-td>Menace</b-td>
-            <b-td>3</b-td>
-            <b-td>
-              <figure>
-                <img src="../assets/trophy.png" />
-                <figcaption>2011</figcaption>
-              </figure>
-              <figure>
-                <img src="../assets/trophy.png" />
-                <figcaption>2015</figcaption>
-              </figure>
-            </b-td>
-          </b-tr>
-          <b-tr class="align-middle">
-            <b-td>Stout</b-td>
-            <b-td>3</b-td>
-            <b-td>
-              <figure>
-                <img src="../assets/trophy.png" />
-                <figcaption>1998</figcaption>
-              </figure>
-              <figure>
-                <img src="../assets/trophy.png" />
-                <figcaption>2013</figcaption>
-              </figure>
-            </b-td>
-          </b-tr>
-          <b-tr class="align-middle">
-            <b-td>Blank</b-td>
-            <b-td>2</b-td>
-            <b-td>
-              <figure>
-                <img src="../assets/trophy.png" />
-                <figcaption>2007</figcaption>
-              </figure>
-            </b-td>
-          </b-tr>
-          <b-tr class="align-middle">
-            <b-td>Makaveli</b-td>
-            <b-td>2</b-td>
-            <b-td>
-              <figure>
-                <img src="../assets/trophy.png" />
-                <figcaption>2000</figcaption>
-              </figure>
-            </b-td>
-          </b-tr>
-          <b-tr class="align-middle">
-            <b-td>Outta Control</b-td>
-            <b-td>2</b-td>
-            <b-td>
-              <figure>
-                <img src="../assets/trophy.png" />
-                <figcaption>2012</figcaption>
-              </figure>
-            </b-td>
-          </b-tr>
-          <b-tr class="align-middle">
-            <b-td>Showtime</b-td>
-            <b-td>3</b-td>
-            <b-td>
-              <figure>
-                <img src="../assets/trophy.png" />
-                <figcaption>2017</figcaption>
-              </figure>
-            </b-td>
-          </b-tr>
-          <b-tr class="align-middle">
-            <b-td>Burghman</b-td>
-            <b-td>1</b-td>
-            <b-td></b-td>
-          </b-tr>
-          <b-tr class="align-middle">
-            <b-td>Gunslingers</b-td>
-            <b-td>2</b-td>
-            <b-td></b-td>
-          </b-tr>
-          <b-tr class="align-middle">
-            <b-td>Jamaica's Finest</b-td>
-            <b-td>3</b-td>
-            <b-td></b-td>
-          </b-tr>
-          <b-tr class="align-middle">
-            <b-td>Junkyard Dawgs</b-td>
-            <b-td>0</b-td>
-            <b-td></b-td>
           </b-tr>
         </b-tbody>
       </b-table-simple>
-    </b-container>-->
+    </b-container>
   </div>
 </template>
 
@@ -201,6 +40,6 @@ figure {
   display: inline-block;
   margin: 0;
   text-align: center;
-  padding: 0;
+  padding-top: 20px;
 }
 </style>
